2e3540e0becbbf88d02e6012a8b1cc55f88631ad
Orçun Çolak <orcun.colak@hazelcast.com>
2023-11-03 10:42:14 +0300

    use flyway

--- src/main/resources/db/migration/V1__employee.sql
+++ src/main/resources/db/migration/V1__employee.sql
@@ -1,3 +1,10 @@
+CREATE TABLE employee (
+  id BIGINT NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  CONSTRAINT pk_employee PRIMARY KEY (id)
+);
+
 INSERT INTO employee (id,first_name,last_name) VALUES (1, 'employee1','lastname1');
 INSERT INTO employee (id,first_name,last_name) VALUES (2, 'employee2','lastname2');
 INSERT INTO employee (id,first_name,last_name) VALUES (3, 'employee3','lastname3');
--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE employee (
-  id BIGINT NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  CONSTRAINT pk_employee PRIMARY KEY (id)
-);
